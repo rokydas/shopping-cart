3ad919dba8ba1b507b76b81b8595c1d19de9193a
Roky Das <droky136@gmail.com>
2020-07-24 01:28:06 +0600

    NaN for empty input and string  solved

--- script.js
+++ script.js
@@ -3,10 +3,37 @@ let product1Minus = document.getElementById('product1-minus');
 let product2Plus = document.getElementById('product2-plus');
 let product2Minus = document.getElementById('product2-minus');
 
-function priceByInput(inputId, priceId, unitPrice){
-    const input = parseFloat(document.getElementById(inputId).value);
-    const price = document.getElementById(priceId);
-    price.innerText = (unitPrice * input);    
+function totalPriceCalculator(price, otherPrice) {
+    totalPrice = price + otherPrice;
+    let tax = totalPrice * 0.05;
+    let totalWithTax = totalPrice + tax;
+    if (Math.round(totalPrice) - totalPrice == 0) document.getElementById("total-price").innerHTML = totalPrice;
+    else document.getElementById("total-price").innerHTML = totalPrice.toFixed(2);
+
+    if (Math.round(tax) - tax == 0) document.getElementById("tax").innerHTML = tax;
+    else document.getElementById("tax").innerHTML = tax.toFixed(2);
+
+    if (Math.round(totalWithTax) - totalWithTax == 0) document.getElementById("total-with-tax").innerHTML = totalWithTax;
+    else document.getElementById("total-with-tax").innerHTML = totalWithTax.toFixed(2);
+}
+
+function priceByInput(inputId, priceId, unitPrice) {
+    let price = parseFloat(document.getElementById(priceId).innerHTML);
+    let totalPrice = parseFloat(document.getElementById("total-price").innerHTML);
+    let otherPrice = totalPrice - price;
+
+    let inputAsString = document.getElementById(inputId).value;
+    if(inputAsString == "") inputAsString = "0";
+    console.log(isNaN(inputAsString));
+    if (isNaN(inputAsString) == false) {
+        input = parseFloat(inputAsString);
+        console.log(input);
+        price = unitPrice * input;
+        document.getElementById(priceId).innerText = price;
+
+        totalPriceCalculator(price, otherPrice);
+    }
+
 }
 
 function numCount(inputId, sign) {
@@ -15,7 +42,7 @@ function numCount(inputId, sign) {
         if (sign == true) productCount++;
         else productCount--;
         document.getElementById(inputId).value = productCount;
-        console.log(productCount);
+        // console.log(productCount);
     }
 }
 
@@ -23,20 +50,17 @@ function priceCount(inputId, priceId, unitPrice) {
     let price = parseFloat(document.getElementById(priceId).innerHTML);
     let input = parseInt(document.getElementById(inputId).value);
     let totalPrice = parseFloat(document.getElementById("total-price").innerHTML);
-    let tax = parseFloat(document.getElementById("tax").innerHTML);
+
     let otherPrice = totalPrice - price;
     // console.log("price", price);
     // console.log("input", input);
     if (!(input < 0)) {
         price = unitPrice * input;
-        console.log("Price ", price);
+        // console.log("Price ", price);
         if (Math.round(price) - price == 0) document.getElementById(priceId).innerHTML = price;
         else document.getElementById(priceId).innerHTML = price.toFixed(2);
 
-        totalPrice = price + otherPrice;
-        let totalWithTax = totalPrice + tax;
-        document.getElementById("total-price").innerHTML = totalPrice;
-        document.getElementById("total-with-tax").innerHTML = totalWithTax;
+        totalPriceCalculator(price, otherPrice);
     }
 }
 
